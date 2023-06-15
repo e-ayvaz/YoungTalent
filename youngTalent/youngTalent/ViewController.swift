272fd77c505bd31acdf3ab3514f77933c68562c8
Enes Ayvaz <enes.ayvaz@mobven.com>
2023-06-15 14:23:41 +0300

    created a project

---
+++ youngTalent/youngTalent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youngTalent
+//
+//  Created by Enes Ayvaz on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
